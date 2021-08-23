2fbc2b1e2554c585cf637f6ad21bc38acc4985c9
IsraelSantizo98 <joseisraelss.jiss@gmail.com>
2021-08-22 18:22:11 -0600

    Se modifica varible de nombre de BD

--- php/conection.php
+++ php/conection.php
@@ -3,8 +3,8 @@
     $user = "root";
     $password = "";
     $database = "inventario";
-    $conection = mysqli_connect($server, $user, $database, $password);
-    if(!$conection == true){
+    $conection = mysqli_connect($server, $user, $password, $database);
+    if($conection == true){
         echo "Conexion exitosa";
     }else{
         echo "Error en conexion";
